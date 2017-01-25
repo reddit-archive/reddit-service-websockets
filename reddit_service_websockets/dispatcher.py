4f0fa421b98c884e57da036db2e2af9e6cc0a833
Daniel Ellis <ellisd23@gmail.com>
2017-01-25 19:04:19 +0000

    Revert "Add support for compression."
    This reverts commit 09b85770692e7d827ed7efaa668943f4812f03d1.

--- reddit_service_websockets/dispatcher.py
+++ reddit_service_websockets/dispatcher.py
@@ -1,47 +1,8 @@
-from collections import namedtuple
 import posixpath
 import random
-from zlib import (
-    compressobj,
-    DEFLATED,
-    MAX_WBITS,
-)
 
 import gevent
 import gevent.queue
-
-from .patched_websocket import make_compressed_frame
-
-
-# See http://www.zlib.net/manual.html#Advanced for details:
-#
-#     "windowBits can also be -8..-15 for raw deflate"
-#
-# Also see http://stackoverflow.com/a/22311297/720638
-#
-# We use a single global compressor to save on memory overhead, at the expense
-# of compression efficiency (since we cannot maintain a per-connection context
-# window).
-
-COMPRESSOR = compressobj(7, DEFLATED, -MAX_WBITS)
-
-
-# We don't bother compressing if the message would already fit into a single
-# packet (i.e. if the length of the message including IP and TCP headers is
-# less than the Maximum Transmission Unit).
-#
-# According to this, the MTU of a packet includes IP and TCP but not ethernet
-# headers:
-#
-#     http://stackoverflow.com/a/9151421/720638
-#
-# 1500 - common MTU size
-# 60 - TCP max header size
-# 60 - IP max header size
-MIN_COMPRESS_SIZE = 1500 - 60 - 60
-
-
-Message = namedtuple('Message', ['compressed', 'raw'])
 
 
 def _walk_namespace_hierarchy(namespace):
@@ -60,13 +21,6 @@ class MessageDispatcher(object):
 
     def on_message_received(self, namespace, message):
         consumers = self.consumers.get(namespace, [])
-
-        # Compress the message
-        if len(message) >= MIN_COMPRESS_SIZE:
-            compressed = make_compressed_frame(message, COMPRESSOR)
-        else:
-            compressed = None
-        message = Message(compressed=compressed, raw=message)
 
         with self.metrics.timer("dispatch"):
             for consumer in consumers:
